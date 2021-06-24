59bc813ebb6e3ac1236130df40848fec4391a294
lucas7788 <sishsh@163.com>
2021-06-24 16:57:55 +0800

    update test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,7 +1,7 @@
 const {expect} = require("chai");
 
 describe("RedPacket", function () {
-    let tokenDemo, redPacket, owner, acct1;
+    let tokenDemo, redPacket, owner, acct1, assetAmount;
     beforeEach(async function () {
         const TokenDemo = await ethers.getContractFactory("TokenDemo");
         tokenDemo = await TokenDemo.deploy(10000000, "L Token", 18, "LT");
@@ -9,8 +9,8 @@ describe("RedPacket", function () {
         const RedPacket = await ethers.getContractFactory("RedPacket");
         redPacket = await RedPacket.deploy(tokenDemo.address);
         await redPacket.deployed();
-        console.log("redPacket.address:", redPacket.address);
         [owner, acct1] = await ethers.getSigners();
+        assetAmount = 1000;
     });
     it("balance", async function () {
         let balance = await acct1.getBalance();
@@ -22,20 +22,19 @@ describe("RedPacket", function () {
         expect(await redPacket.token()).to.equal(tokenDemo.address);
     });
     it("sendRedPacket", async function () {
-        balance = await tokenDemo.balanceOf(owner.address)
-        console.log("owner before balance:", balance.toString());
-        const approveTx = await tokenDemo.approve(redPacket.address, 100);
+        let beforeBalance = await tokenDemo.balanceOf(owner.address)
+        const approveTx = await tokenDemo.approve(redPacket.address, assetAmount);
         await approveTx.wait();
-        const sendRedPacketTx = await redPacket.sendRedPacket(100, 10);
+        const sendRedPacketTx = await redPacket.sendRedPacket(assetAmount, 10);
         await sendRedPacketTx.wait();
-        balance = await tokenDemo.balanceOf(owner.address);
-        console.log("owner after balance:", balance.toString());
+        let afterBalance = await tokenDemo.balanceOf(owner.address);
+        expect(afterBalance-beforeBalance).to.equal(assetAmount);
     });
     it("after sendRedPacket", async function () {
         res = await redPacket.packets(0);
-        console.log("packets:", res);
+        expect(res.receivedIndex).to.equal(0);
         res = await redPacket.nextPacketId();
-        console.log("nextPacketId:", res);
+        expect(res).to.equal(1);
     });
     it("receivePacket", async function () {
         balance = await tokenDemo.balanceOf(acct1.address);
